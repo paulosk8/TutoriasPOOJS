1577b71358c81dc5e22bb470dfea549230dc98f0
Paulo Galarza <paulo-123@hotmail.es>
2025-02-21 22:08:25 -0500

    Dia 3: Polimorfismo

--- Dia 2/alumno.js
+++ Dia 2/alumno.js
@@ -1,7 +1,16 @@
-const Persona = require("./persona.mjs");
+import Persona from "./persona.mjs";
 // Clase hijo de Persona
 class Alumno extends Persona {
-  constructor(curso) {
+  constructor(nombre, edad, curso) {
+    super(nombre, edad);
     this.curso = curso;
   }
+  saludar() {
+    console.log(`Hola, soy ${this.nombre} y estudio en el curso: ${this.curso}`);
+  }
 }
+
+const alumno1 = new Alumno("Paulo", 31, "Primero DS");
+alumno1.saludar();
+const persona1 = new Persona("Juan", 30);
+persona1.saludar();
